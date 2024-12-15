26c2066c6122596d7bda525964bcfe933a56d227
neiroc-02 <neiro.cabrera@yahoo.com>
2024-12-14 16:12:22 -0800

    Day 2 setup

---
+++ day2/day2.go
@@ -0,0 +1,19 @@
+/* day1.go */
+/* https://adventofcode.com/2024/day/2 */
+/* https://learnxinyminutes.com/go/ */
+package main
+
+/*
+import (
+	"fmt"
+	"os"
+	"sort"
+	"bufio"
+	"strconv"
+	"string"
+)
+*/
+
+func main() {
+
+}
