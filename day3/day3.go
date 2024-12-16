874193333d8cdc866304941d3115cd7a6b95506a
neiroc-02 <neiro.cabrera@yahoo.com>
2024-12-16 13:57:02 -0800

    Finished day3

--- day3/day3.go
+++ day3/day3.go
@@ -11,15 +11,29 @@ import (
 	"strconv"
 )
 
-// "strconv"
-func evaluate_muls(valid_muls []string) int {
+func evaluate_muls(valid_muls []string, can_run *bool) int {
 	total := 0
 	pattern := regexp.MustCompile(`[0-9]+`)
 	for idx := range len(valid_muls) {
-		nums := pattern.FindAllString(valid_muls[idx], -1)
-		num1, _ := strconv.Atoi(nums[0])
-		num2, _ := strconv.Atoi(nums[1])
-		total += num1 * num2
+		if valid_muls[idx] == "do()" {
+			*can_run = true
+			continue
+		} else if valid_muls[idx] == "don't()" {
+			*can_run = false
+			continue
+		} else if *can_run {
+			nums := pattern.FindAllString(valid_muls[idx], -1)
+			if len(nums) == 2 {
+				num1, err1 := strconv.Atoi(nums[0])
+				num2, err2 := strconv.Atoi(nums[1])
+				if err1 != nil || err2 != nil {
+					fmt.Println(err1)
+					fmt.Println(err2)
+					return -1
+				}
+				total += num1 * num2
+			}
+		}
 	}
 	return total
 }
@@ -31,12 +45,14 @@ func main() {
 		return
 	}
 	total := 0
+	can_run := true
 	scanner := bufio.NewScanner(file)
-	pattern := regexp.MustCompile(`mul\([0-9]+,[0-9]+\)`) //regex for mul(num,num)
+	//pattern1 := regexp.MustCompile(`mul\([0-9]+,[0-9]+\)`) //use this pattern to get part1 answer
+	pattern3 := regexp.MustCompile(`(mul\([0-9]+,[0-9]+\))|(do\(\))|(don't\(\))`)
 	for scanner.Scan() {
 		line := scanner.Text()
-		valid_muls := pattern.FindAllString(line, -1)
-		total += evaluate_muls(valid_muls)
+		valid_muls := pattern3.FindAllString(line, -1)
+		total += evaluate_muls(valid_muls, &can_run)
 	}
 	fmt.Println(total)
 	return
